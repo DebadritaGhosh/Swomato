fbe8694faf07537b106f8f4623ef7beac0054dc1
DebadritaGhosh <debadrita.ghosh2010@gmail.com>
2025-09-14 09:16:37 +0530

    feat: integrate Appwrite SDK and initial configuration

---
+++ lib/appwrite.ts
@@ -0,0 +1,8 @@
+export const appwriteConfig = {
+  endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
+  projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+  projectName: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_NAME,
+  platform: "com.developedbydeb.swomato",
+  databaseId : "68c628ff000b33eb0051",
+  userCollectionId : "user"
+};
