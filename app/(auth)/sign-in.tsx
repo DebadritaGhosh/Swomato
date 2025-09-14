f7dd11c3a4a648d734e5110120505f1f8ac2f5d9
DebadritaGhosh <debadrita.ghosh2010@gmail.com>
2025-09-14 09:43:05 +0530

    feat: implement Sign In page UI with styling

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,10 +1,37 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import CustomButton from "@/components/CustomButton";
+import CustomInput from "@/components/CustomInput";
+import { Link } from "expo-router";
+import React from "react";
+import { Text, View } from "react-native";
 
 export default function SignIn() {
   return (
-    <View>
-      <Text>sign-in</Text>
+    <View className="gap-10 bg-white rounded-lg p-5 mt-5">
+      <CustomInput
+        placeholder="Enter your email"
+        value={""}
+        onChangeText={(text) => {}}
+        label="Email"
+        keyboardType="email-address"
+      />
+      <CustomInput
+        placeholder="Enter your password"
+        value={""}
+        onChangeText={(text) => {}}
+        label="Password"
+        secureTextEntry={true}
+      />
+
+      <CustomButton title="Sign In" />
+
+      <View className="flex justify-center mt-5 flex-row gap-2">
+        <Text className="base-regular text-gray-100">
+          {"Don't have an account?"}
+        </Text>
+        <Link href="/sign-up" className="base-bold text-primary">
+          Sign Up
+        </Link>
+      </View>
     </View>
-  )
+  );
 }
